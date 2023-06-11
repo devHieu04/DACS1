d8f76e3b9743aef8d4e9439fa5393ec87d75f15e
Nguyễn Duy Hiếu <nguyenduyhieu@MacBook-Pro-cua-Nguyen.local>
2023-06-11 20:42:54 +0700

    update by Hieu

---
+++ JS/home.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarNav = document.querySelector(".navbar-nav");
+  
+    navbarToggler.addEventListener("click", function () {
+      navbarNav.classList.toggle("show");
+    });
+  });
+  const toggleSwitch = document.querySelector('.dark-mode-toggle');
+
+  toggleSwitch.addEventListener('click', switchTheme);
+  
+  function switchTheme() {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    const toggleImg = document.querySelector('.dark-mode-toggle');
+  
+    if (isDarkMode) {
+      toggleImg.src = '/IMG/sun.png';
+    } else {
+      toggleImg.src = '/IMG/night.png';
+    }
+  }
+  
+  
+  
